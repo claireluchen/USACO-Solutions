1b461ac7279daf6ee20094e12341caed6bd90813
claireluchen <81056267+claireluchen@users.noreply.github.com>
2021-03-31 21:47:15 -0400

    Update Maximum Distance.java

--- Guide/Bronze/Basic Complete Search/Maximum Distance.java
+++ Guide/Bronze/Basic Complete Search/Maximum Distance.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class Main{
-
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     int N = in.nextInt();
@@ -22,5 +21,4 @@ public class Main{
     }
     System.out.println((int) maxDistance);
   }
-
 }
