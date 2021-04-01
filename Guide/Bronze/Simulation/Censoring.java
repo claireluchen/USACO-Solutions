7c9a8d2a1e27390f2acf1e686bb8537e8514917c
claireluchen <81056267+claireluchen@users.noreply.github.com>
2021-03-31 21:48:17 -0400

    Update Censoring.java

--- Guide/Bronze/Simulation/Censoring.java
+++ Guide/Bronze/Simulation/Censoring.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class Main{
-
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     String text = in.next();
@@ -10,5 +9,4 @@ public class Main{
     }
     System.out.println(text);
   }
-
 }
