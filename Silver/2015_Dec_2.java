1c3be9742e0fce2cce4e25390bd379924e44c5d1
claireluchen <81056267+claireluchen@users.noreply.github.com>
2021-06-06 13:29:23 -0400

    Create 2015_Dec_2.java

---
+++ Silver/2015_Dec_2.java
@@ -0,0 +1,45 @@
+//greedy with sorting (card problem)
+import java.util.*;
+import java.io.*;
+
+public class Test {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+  public static void main(String[] args) throws IOException{
+	    int n = readInt();
+	    HashSet<Integer> set = new HashSet<>();
+	    for (int i = 0; i < n; i++) set.add(readInt());
+	    ArrayList<Integer> b = new ArrayList<>();
+	    ArrayList<Integer> e = new ArrayList<>();
+	    for (int i = 1; i <= 2 * n; i++) {
+	    	if (set.contains(i)) {
+	    		e.add(i);
+	    	}else {
+	    		b.add(i);
+	    	}
+	    }
+	    
+	    int points = 0;
+	    int bIndex = 0, eIndex = 0;
+	    while (bIndex < n && eIndex < n) {
+	    	if (b.get(bIndex) < e.get(eIndex)) {
+	    		bIndex++;
+	    	}else {
+	    		bIndex++;
+	    		eIndex++;
+	    		points++;
+	    	}
+	    }
+	    System.out.println(points);
+	}
+}
