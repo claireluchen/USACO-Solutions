4fd9447166dfa2585f2d1b619d02d8c3e0c9e5ad
claireluchen <81056267+claireluchen@users.noreply.github.com>
2021-06-06 12:17:12 -0400

    Create 2018_Open_2.java

---
+++ Silver/2018_Open_2.java
@@ -0,0 +1,33 @@
+//greeding with sorting
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	static String next () throws IOException {
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static int readInt () throws IOException {
+		return Integer.parseInt(next());
+	}
+
+	public static void main(String[] args) throws IOException{
+    int n = readInt();
+    int[] A = new int[n];
+    for (int i = 0; i < n; i++) A[i] = readInt();
+    Arrays.sort(A);
+    int inLine = 0;
+    for (int i = n - 1; i >= 0; i--){
+      if (A[i] >= inLine){
+        inLine++;
+      }else{
+        break;
+      }
+    }
+    System.out.println(inLine);
+  }
+}
