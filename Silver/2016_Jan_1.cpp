eb97019370f09963031eda172e83bb9b6da19406
claireluchen <81056267+claireluchen@users.noreply.github.com>
2021-07-17 20:22:45 -0400

    Create 2016_Jan_1.cpp

---
+++ Silver/2016_Jan_1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn = 5e4 + 5;
+
+int n, k;
+int arr[maxn];
+
+bool check(int r){
+  int cnt = 1;
+  int left = arr[0], right = left + 2 * r;
+  for (int i = 1; i < n; i++){
+    if (arr[i] > right){
+      cnt++;
+      left = arr[i];
+      right = left + 2 * r;
+    }
+  }
+  if (cnt <= k) return true;
+  return false;
+}
+
+int binarySearch(int low, int high){
+  if (low == high) return low;
+  if (low + 1 == high){
+    if (!check(low)) return high;
+    else return low;
+  }
+  int mid = (low + high) / 2;
+  cout << mid <<"\n";
+  if (!check(mid)) return binarySearch(mid + 1, high);
+  else return binarySearch(low, mid);  
+}
+
+int main() {  
+  cin >> n >> k;
+  for (int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+
+  sort(arr, arr + n);
+  int low = 0, high = arr[n - 1];
+  cout << binarySearch(low, high) << endl;
+  return 0;
+}
